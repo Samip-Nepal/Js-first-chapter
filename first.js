e04226d9b1ca458d5630cc635cc8908ccd8c8186
Samip-Nepal <samipnepal5@email.com>
2024-01-26 00:07:21 +0545

    Adding let,const,var before variable and example in it

--- first.js
+++ first.js
@@ -2,3 +2,21 @@ Console="Samip";
 console.log(Console);
 isfull=25;
 console.log(isfull);
+// Different use of var ,let and const
+var age=25;
+var age=30;
+var age=45;
+console.log(age);
+output=45;
+// Here in var Different variable has Different memory space but in let
+let age=25;
+age=30;
+age=45;
+console.log(age); 
+output=45;
+// Here the let as same memory space and update when it change
+const age=25;
+age=30;
+age=45;
+console.log(age);
+// Here comes error in output
