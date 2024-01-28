22d4a663c5961f8e1a61f1aa44dce17fe5515dbc
Samip-Nepal <samipnepal5@email.com>
2024-01-28 22:39:14 +0545

    Adding primitive data type

--- first.js
+++ first.js
@@ -1,22 +1,7 @@
-Console="Samip";
-console.log(Console);
-isfull=25;
-console.log(isfull);
-// Different use of var ,let and const
-var age=25;
-var age=30;
-var age=45;
-console.log(age);
-output=45;
-// Here in var Different variable has Different memory space but in let
 let age=25;
-age=30;
-age=45;
-console.log(age); 
-output=45;
-// Here the let as same memory space and update when it change
-const age=25;
-age=30;
-age=45;
-console.log(age);
-// Here comes error in output
+let fullName="Samip";
+let don=true;
+let ages;
+let fullname=null;
+let x=BigInt("123");
+let y=Symbol("hello");
